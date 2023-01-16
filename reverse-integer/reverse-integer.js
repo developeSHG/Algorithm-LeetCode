e7d203315163d793b2409200b6c331d5b246c04e
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-16 10:11:29 +0900

    Time: 83 ms, Memory: 44 MB - LeetHub

--- reverse-integer/reverse-integer.js
+++ reverse-integer/reverse-integer.js
@@ -1,14 +1,22 @@
-var reverse = function(x) {
-    if(x===0)return 0;
-    if(x>0){
-        let result = parseInt(x.toString().split('').reverse().join(''));
-        return result < Math.pow(2,31)-1?result:0;
-    }else{
-        let result = x.toString().split('');
-        result.push('-');
-        result.shift();
-        result = result.reverse()
-        result = parseInt(result.join(''));
-        return result > Math.pow(-2,31)?result:0;
-    }
+/**
+ * @param {number} x
+ * @return {number}
+ */
+function t1(x) {
+    let result = parseInt(x.toString().split("").reverse().join(""));
+    return result < Math.pow(2, 31) - 1 ? result : 0;
+}
+
+function t2(x) {
+    let result = x.toString().split("");
+    result.push("-");
+    result.shift();
+    result.reverse();
+    result = parseInt(result.join(""));
+    return result > Math.pow(-2, 31) ? result : 0;
+}
+
+var reverse = function (x) {
+    if (x === 0) return 0;
+    return (x > 0 ? t1 : t2)(x);
 };
