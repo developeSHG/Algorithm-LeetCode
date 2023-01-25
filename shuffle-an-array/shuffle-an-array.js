385d53d62a38adb256b8207b9ac0ca4bdfec9505
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-25 13:58:19 +0900

    Time: 163 ms, Memory: 51.5 MB - LeetHub

--- shuffle-an-array/shuffle-an-array.js
+++ shuffle-an-array/shuffle-an-array.js
@@ -18,13 +18,11 @@ Solution.prototype.reset = function() {
  * @return {number[]}
  */
 Solution.prototype.shuffle = function() {
-        for(let i = this.nums.length - 1; i > 0; i--){
-            const random = Math.floor(Math.random() * (i + 1));
-            
-            [this.nums[i], this.nums[random]] = [this.nums[random], this.nums[i]];
-        }
-
-        return this.nums;
+    for(let i = this.nums.length - 1; i > 0; i--) {
+        const random = Math.floor(Math.random() * (i + 1));
+        [this.nums[i], this.nums[random]] = [this.nums[random], this.nums[i]];
+    }
+    return this.nums;
 };
 
 /** 
