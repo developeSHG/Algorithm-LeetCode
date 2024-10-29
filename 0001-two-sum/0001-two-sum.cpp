0cfd8d69436f395f4246fdac3f9c019f161802fc
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2024-10-29 15:04:09 +0900

    Time: 4 ms (66.69%), Space: 14.4 MB (20.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,12 +5,11 @@ public:
         unordered_map<int, int> m;
         
         for (int i = 0; i < nums.size(); ++i)
-            m.insert({nums[i], i});
-        
-        for (int i = 0; i < nums.size(); ++i)
         {
             int n = target - nums[i];
+            
             if (m.find(n) != m.end() && i != m[n]) return { i, m[n] };
+            else m.insert({nums[i], i});
         } 
         return {0, 0};
     }
